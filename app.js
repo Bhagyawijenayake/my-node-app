fec83e388ae7278666277bc1d5a5d7aac63fb7f4
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2025-06-28 19:44:15 +0530

    change text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js Docker Apppf!");
+  res.send("Hello from Node.js Docker Apppff!");
 });
 
 const PORT = process.env.PORT || 3000;
