79af923fac6d14ebbd81e097a4495e24ef27423e
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2025-07-06 20:33:32 +0530

    modified:   __tests__/app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,6 +6,6 @@ describe("GET /", () => {
   it("should return hello message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello from Node.js Docker Apppff!");
+    expect(res.text).toBe("Hello from Node.js Docker App test!");
   });
 });
